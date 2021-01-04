c815c4c1dd3db20d575e29b2a84caf1d91c3bb61
Bruno1083 <cavalcante1083@gmail.com>
2021-01-04 00:28:44 -0300

    Classe controller

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -1,7 +1,33 @@
+using ApiPessoas.Data.Collections;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
 namespace ApiPessoas.Controllers
 {
-    public class PessoaController
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
     {
-        
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Pessoa> _infectadosCollection;   
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Pessoa>(typeof(Pessoa).Name.ToLower());
+        }
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody]PessoaDto dto)
+        {
+            var pessoa = new Pessoa(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+            //Adicionar no banco
+            _infectadosCollection.InsertOne(pessoa);
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Pessoa>.Filter.Empty).ToList();
+            return Ok(infectados);
+        }
     }
 }
